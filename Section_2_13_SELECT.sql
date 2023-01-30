e7db33be691898821b96f7040d97395c3d67741f
Ilya Biryukov <is.vaso@gmail.com>
2023-01-30 15:38:00 +0600

    Section_2_13_SELECT passed

---
+++ Section_2_13_SELECT.sql
@@ -0,0 +1,7 @@
+-- Challenges from Udemy.com course:
+-- The Complete SQL Bootcamp: Go from Zero to Hero
+
+-- Challenge: SELECT
+-- Situation: We want to send out a promotional email to our existing customers!
+
+SELECT first_name, last_name, email FROM customer;
